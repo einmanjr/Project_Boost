a81451ac7870845866d341e904fadfaa5a42e08a
meinman <einmanjr@gmail.com>
2020-03-30 16:24:09 -0700

    Level 1 Rocket + script

---
+++ Project_Boost/Assets/Scenes/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+   void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("Thrusting");
+        }
+
+        if(Input.GetKey(KeyCode.D)) //separate statement- can simulaneous directions
+        {
+            print("Rotate Left");
+        }
+
+        if(Input.GetKey(KeyCode.F))
+        {
+            print("Rotate Right");
+        }
+    }
+}
