43c990250d976c349657aa3182290ce715b78d7b
meinman <einmanjr@gmail.com>
2020-03-30 22:49:27 -0700

    Moving along level 1

--- Project_Boost/Assets/Scenes/Rocket.cs
+++ Project_Boost/Assets/Scenes/Rocket.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f; //RCS-- Reacion Control System
+
     Rigidbody rigidBody;
     AudioSource audioSource;
     void Start()
@@ -16,15 +19,35 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrusting();
+        Rotation();
     }
 
-   void ProcessInput()
+    void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Fuel":
+                print("Fuel up!");
+                break;
+            default:
+                print("Death by Explosion!");
+                //TODO -- kill player
+                break;
+        }
+
+    }
+
+    #region controls
+    private void Thrusting()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            // print("Thrusting");
-            rigidBody.AddRelativeForce(Vector3.up);
+            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)  //so it doesn't layer
             {
                 audioSource.Play();
@@ -34,19 +57,35 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
+    }
+    void Rotation()
+    {
+        //Thrusting();
+
+        rigidBody.freezeRotation = true; // take manual control of rotation;
+
         
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
 
-        if(Input.GetKey(KeyCode.D)) //separate statement- can simulaneous directions
+        if (Input.GetKey(KeyCode.D)) //separate statement- can simulaneous directions
         {
+            
             print("Rotate Left");
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
- 
-        if(Input.GetKey(KeyCode.F))
+
+        if (Input.GetKey(KeyCode.F))
         {
             print("Rotate Right");
-            transform.Rotate(Vector3.back);
+            transform.Rotate(Vector3.back * rotationThisFrame);
         }
+
+        rigidBody.freezeRotation = false; // resume physics of rotation;
+
+
     }
+
+    #endregion
+
 }
