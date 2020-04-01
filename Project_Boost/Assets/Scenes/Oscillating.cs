e5f12bef99d802332f8e1005ea1b2018f2e27a72
meinman <einmanjr@gmail.com>
2020-03-31 23:04:48 -0700

    update 10.2.5.3.5439 - Added Oscillating Objects

---
+++ Project_Boost/Assets/Scenes/Oscillating.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillating : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 0f, 0f);
+    [SerializeField] float period = 6f;
+
+    //todo remove from inspector later
+    // [Range(0,1)] [SerializeField] 
+    float movementFactor; //0 not moved, 1 fully moved
+
+    Vector3 startingPos;
+    
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(period <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / period; // grows continuall from 0
+
+        const float tau = Mathf.PI * 2f; //about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+
+        movementFactor = rawSinWave / 2f + 0.5f; ;
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+    }
+}
--- Project_Boost/Assets/Scenes/Rocket.cs
+++ Project_Boost/Assets/Scenes/Rocket.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
+ 
+
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 100f;
-    [SerializeField] float mainThrust = 100f; //RCS-- Reacion Control System
+    [SerializeField] float mainThrust = 100f;//RCS-- Reacion Control System
+    [SerializeField] float levelLoadDelay = 2f;
+   
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip success;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem successParticles;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending}
+    State state = State.Alive;
+    
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
@@ -17,51 +32,93 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrusting();
-        Rotation();
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        switch(collision.gameObject.tag)
+        if (state != State.Alive) { return; } // ignore collisions on death
+
+        switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK");
+                //top off fuel
                 break;
             case "Finish":
-                print("Hit Finish");
-                SceneManager.LoadScene(1);
+                successSequence();
+                //parameterize time
                 break;
             case "Fuel":
                 print("Fuel up!");
                 break;
             default:
-                print("Dead AF");
-                SceneManager.LoadScene(0);
-                //TODO -- kill player
+                deathSequence();
                 break;
         }
 
     }
 
+    private void successSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        successParticles.Play();
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void deathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        mainEngineParticles.Stop();
+        deathParticles.Play();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstLevel", levelLoadDelay);
+        //parameterize time
+        //TODO -- kill player
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1); //TODO allow for more than 2 levels
+    }
+
     #region controls
-    private void Thrusting()
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            print("Thrusting");
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            if (!audioSource.isPlaying)  //so it doesn't layer
-            {
-                audioSource.Play();
-            }
+            ApplyThrust();
         }
         else
         {
             audioSource.Stop();
+            mainEngineParticles.Stop();
         }
     }
-    void Rotation()
+
+    private void ApplyThrust()
+    {
+        print("Thrusting");
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audioSource.isPlaying)  //so it doesn't layer
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play();
+    }
+
+    void RespondToRotateInput()
     {
         //Thrusting();
 
